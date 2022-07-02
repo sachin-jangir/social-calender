0cee506926cc9ffa532389c6f17a0a971aa064a3
sachin-jangir <99168247+sachin-jangir@users.noreply.github.com>
2022-07-02 17:58:42 +0530

    some commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import {useState, useEffect} from "react";
+import Page from "./components/Page";
+
+
+
 
 function App() {
+
+  const [monthData, setMonthData] = useState([]); 
+
+  useEffect(() => {
+    const getMonthData = async () => {
+      const dataFromApi = await fetchMonthData()
+      console.log(dataFromApi)
+      setMonthData(dataFromApi)
+    }
+
+    // getMonthData()
+  }, [])
+
+  // fetch month data
+  const fetchMonthData = async (month="July") => {
+    const res = await fetch("https://v1.igpods.com/api/social_calendar/get/" + month)
+    const data = await res.json()
+    return data;
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Page />
   );
 }
+
+
 
 export default App;
